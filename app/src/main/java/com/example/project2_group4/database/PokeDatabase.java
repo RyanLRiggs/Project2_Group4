ed12d2268c241f5317d4d9e4027c591a124629a0
FernandoVelarde <fvelarde@csumb.edu>
2025-11-15 21:56:17 -0800

    Created prepopulation call back for the test users. User1 and User2.

--- app/src/main/java/com/example/project2_group4/database/PokeDatabase.java
+++ app/src/main/java/com/example/project2_group4/database/PokeDatabase.java
@@ -2,9 +2,11 @@ package com.example.project2_group4.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {User.class}, version = 1)
 public class PokeDatabase extends RoomDatabase {
@@ -18,5 +20,23 @@ public class PokeDatabase extends RoomDatabase {
         }
         return instance;
     }
+
+    private static RoomDatabase.Callback prepopulateCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            new Thread(() -> {
+                UserDAO userDAO = instance.userDAO();
+
+                if(userDAO.getUserCount() == 0) {
+                    User user1 = new User("testuser1", "testuser1", false);
+                    User user2 = new User("admin1", "admin1", true);
+                    userDAO.insertUser(user1);
+                    userDAO.insertUser(user2);
+                }
+            }).start();
+        }
+    };
 
 }
