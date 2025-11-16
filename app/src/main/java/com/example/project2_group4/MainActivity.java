c17d9e4c5b854e2e2b01cce497176b441913fc33
RyanLRiggs <rriggs@csumb.edu>
2025-11-15 16:55:08 -0800

    made MainActivity and started LoginActivity

--- app/src/main/java/com/example/project2_group4/MainActivity.java
+++ app/src/main/java/com/example/project2_group4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project2_group4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.project2_group4.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.MainLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = LoginActivity.mainToLogin(getApplicationContext());
+                startActivity(intent);
+            }
         });
+
+
+
+
     }
 }
