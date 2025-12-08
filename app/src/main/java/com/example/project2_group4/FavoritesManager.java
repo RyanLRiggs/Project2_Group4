84bd7af6ebc78349ec7644f5a31c9bab0b9cefe5
Sheepling <juliakroug@gmail.com>
2025-12-07 22:27:57 -0800

    Manager working?

---
+++ app/src/main/java/com/example/project2_group4/FavoritesManager.java
@@ -0,0 +1,29 @@
+package com.example.project2_group4;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FavoritesManager {
+
+    private static final String PREF_NAME = "favorites_prefs";
+    private static final String KEY_FAVORITES = "favorite_pokemon";
+
+    public static void addFavorite(Context context, String pokemonName) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+
+        Set<String> favorites = prefs.getStringSet(KEY_FAVORITES, new HashSet<>());
+        favorites = new HashSet<>(favorites); //new copy made w this
+
+        favorites.add(pokemonName);
+
+        prefs.edit().putStringSet(KEY_FAVORITES, favorites).apply();
+    }
+
+    public static Set<String> getFavorites(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return prefs.getStringSet(KEY_FAVORITES, new HashSet<>());
+    }
+}
