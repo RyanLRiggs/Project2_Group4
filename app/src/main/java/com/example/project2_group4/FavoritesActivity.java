49c9a4556043f91e7441c65c9210d7a588dbe900
Sheepling <juliakroug@gmail.com>
2025-12-07 22:25:29 -0800

    shoukd work? need to fix adapter

--- app/src/main/java/com/example/project2_group4/FavoritesActivity.java
+++ app/src/main/java/com/example/project2_group4/FavoritesActivity.java
@@ -1,11 +1,16 @@
 package com.example.project2_group4;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project2_group4.databinding.ActivityFavoritesBinding;
+
+import java.util.ArrayList;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -17,8 +22,23 @@ public class FavoritesActivity extends AppCompatActivity {
         binding = ActivityFavoritesBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+
+        ArrayList<String> favoriteList =
+                new ArrayList<>(FavoritesManager.getFavorites(this));
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                favoriteList
+        );
+
+
+        binding.favoritesListView.setAdapter(adapter);
     }
-}
-public static Intent landingToFavorites(Context context){
-    return new Intent(context, FavoritesActivity.class);
+
+    public static Intent landingToFavorites(Context context) {
+        return new Intent(context, FavoritesActivity.class);
+    }
 }
